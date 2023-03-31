b2771c26ecece6f6f05cc0b67362b18081e48b76
Premiersoft <premiersoft@Premiersofts-MacBook-Pro-5.local>
2023-03-31 11:42:27 -0300

    refactor

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:rive/rive.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -12,11 +13,56 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-        body: Column(
-      children: [
-        //rive animation
-      ],
+        body: SizedBox(
+      width: size.width,
+      height: size.height,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: size.width,
+              height: 200,
+              child: RiveAnimation.asset(
+                "images/animated_login_character.riv",
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: "E-mail",
+                prefixIcon: Icon(Icons.mail),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Password",
+                prefixIcon: Icon(Icons.lock),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: size.width,
+              child: const Text(
+                "Forgot your password?",
+                textAlign: TextAlign.right,
+                style: TextStyle(decoration: TextDecoration.underline),
+              ),
+            )
+          ],
+        ),
+      ),
     ));
   }
 }
