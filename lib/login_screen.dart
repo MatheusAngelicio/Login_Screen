8a1808502a185de53d2285921158d64fb64fcfe6
Premiersoft <premiersoft@Premiersofts-MacBook-Pro-5.local>
2023-03-31 11:07:07 -0300

    configurar rive

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -12,10 +12,11 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Login Screen"),
-      ),
-    );
+    return Scaffold(
+        body: Column(
+      children: [
+        //rive animation
+      ],
+    ));
   }
 }
